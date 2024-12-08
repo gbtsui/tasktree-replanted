1cac3d8773feca231d12fa79050310a89023fb09
gbtsui <gabriel.tsui@protonmail.com>
2024-12-07 21:51:10 -0500

    chore(aes): prettified page a little bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Logo from "./components/logo";
+import Image from "next/image";
 
 export default function Home() {
   return (
    <div className={"--font-atkinsonHyperlegible"}>
-     <div id="navbar" className="page w-full p-3 flex flex-col items-center justify-end">
+     <div id="navbar" className="w-full p-3 flex items-center justify-between clear-both">
+         <Image
+             src={"./icon.svg"}
+             width={50}
+             height={50}
+             alt={"logo but smol"}></Image>
          <button className={"p-4 text-darkforestgreen bg-eggshell hover:bg-cream color-darkforestgreen transition rounded-bl-xl rounded-tr-xl rounded-br-sm rounded-tl-sm"}>Login</button>
      </div>
      <div id="header" className="page clear-both items-center text-center flex justify-center p-7">
@@ -23,6 +29,12 @@ export default function Home() {
                        <li className={"list-decimal"}>Break down tasks into leaves.</li>
                        <li className={"list-decimal"}>Plant a seed and start growing leaves.</li>
                        <li className={"list-decimal"}>Grow some fruit!</li>
+               </div>
+
+               <div className={"bg-cream text-center m-8 p-10"}>
+                   <p className="text-lg text-gray-600">
+                       More coming soon!
+                   </p>
                </div>
 
                <div className={"text-center w-full p-5"}>
