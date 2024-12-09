db4fb65bae4e0a4f4c914c2235fe98c6d574e762
gbtsui <gabriel.tsui@protonmail.com>
2024-12-09 13:05:41 -0500

    feat(pages): added template dashboard, login form

---
+++ src/app/components/login/loginForm.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function LoginForm() {
+    return (
+        <div>
+            <h1>Login Form</h1>
+        </div>
+    )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Logo from "@/app/components/logo"
+
+export default async function Dashboard() {
+    return (
+        <main>
+            <Logo/>
+            <h1 className={"mb-4 text-xl md:text-2xl"}>
+                Dashboard
+            </h1>
+            <p>add more here idk</p>
+        </main>
+    )
+}
