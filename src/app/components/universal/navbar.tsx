88aacb3878b6434bdca1195cc6fa7cbf0e572dd4
gbtsui <gabriel.tsui@protonmail.com>
2024-12-10 14:21:42 -0500

    fix: added proper login link

--- src/app/components/universal/navbar.tsx
+++ src/app/components/universal/navbar.tsx
@@ -1,6 +1,13 @@
+"use client"
+
 import Image from "next/image";
+import Link from "next/link"
+import {usePathname} from "next/navigation"
+import clsx from "clsx"
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <div id="navbar" className="w-full p-3 flex items-center justify-between clear-both">
             <Image
@@ -8,9 +15,12 @@ export default function Navbar() {
                 width={50}
                 height={50}
                 alt={"logo but smol"}></Image>
-            <button
-                className={"p-4 text-darkforestgreen bg-eggshell hover:bg-cream color-darkforestgreen transition rounded-bl-xl rounded-tr-xl rounded-br-sm rounded-tl-sm"}>Login
-            </button>
+            <Link
+                className={"p-4 text-darkforestgreen bg-eggshell hover:bg-cream color-darkforestgreen transition rounded-bl-xl rounded-tr-xl rounded-br-sm rounded-tl-sm"}
+                href={"/login"}>
+                <p className={"text-xl"}>Login</p>
+            </Link>
+
         </div>
     )
 }
