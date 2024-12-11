cf9f206bbfae381ee23435f291de5bf6f2199903
gbtsui <gabriel.tsui@protonmail.com>
2024-12-11 16:52:50 -0500

    feat: added skeleton signup page
    nonfunctional

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/app/components/universal/navbar";
+
+export default function SignupPage() {
+    return (
+        <div className={"--font-atkinsonHyperlegible text-eggshell"}>
+            <Navbar/>
+            <div id="header" className="page clear-both items-center text-center flex justify-center p-7">
+                <div className="items-center overflow-auto flex justify-center p-7">
+                    <h1 className={"text-9xl mx-0"}>sign up</h1>
+                </div>
+            </div>
+            <div className="page text-center">
+                <main className="page">
+                    <!--<LoginForm/>-->
+                    in progress
+                </main>
+            </div>
+        </div>
+    )
+}
